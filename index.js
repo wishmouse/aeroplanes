5fcb972c3694f8f83b6f98cb99a755f687d7f28b
Wishmouse <heidi.goosen@gmail.com>
2018-11-03 09:18:11 +1300

    getting started

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser')
+var cons = require('consolidate')
+var request = require('superagent')
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'html')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function(req, res){
+  res.render("index")
+})
+
+
+app.listen(3000, function(){
+  console.log("pushing tin .... 3000")
+})
+
+module.exports = app
